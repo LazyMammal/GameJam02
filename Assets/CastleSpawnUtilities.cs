1716581c3723fb6059ff9e99f5b90319b2df5957
panchishin <anton@panchishin.com>
2017-02-18 12:36:36 -0500

    Addition of sequence of play skeleton

---
+++ Assets/CastleSpawnUtilities.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastleSpawnUtilities : MonoBehaviour {
+
+
+	public Material NewFlagMaterial;
+	public Material NewLauncherMaterial;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("This call should be used by the base constructor");
+		Invoke ("ChangeFlagColorExample", 1);		
+	}
+
+	void ChangeFlagColorExample() {
+		ChangeMaterial (NewFlagMaterial, "CastleFlag");
+		ChangeMaterial (NewLauncherMaterial, "Launcher");
+	}
+
+	public void ChangeMaterial(Material material, string tagName) {
+		foreach (Transform childTransform in transform) {
+			GameObject child = childTransform.gameObject;
+			if (child.CompareTag (tagName)) {
+				foreach (Renderer renderer in child.GetComponentsInChildren<Renderer>()) {
+					renderer.material = material;
+				}
+			}
+		}
+	}		
+	
+}
---
+++ Assets/Scripts/SequenceOfPlay.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SequenceOfPlay : MonoBehaviour {
+
+	public GameObject sequenceDisplayText = null;
+
+	// Use this for initialization
+	void Start () {
+		if (sequenceDisplayText == null) {
+			sequenceDisplayText = GameObject.Find ("SequenceOfPlayInfoBox");
+		}
+		NextState ();		
+	}
+
+	public string[] sequenceOfPlay = new string[] { "ClearBattleField", "TitleScreen","GenerateBattleField",
+		"BattleFieldFlyOver","Bidding","StartSimulation","DistributeWin"};
+
+	public int state = -1;
+	public void NextState() {
+		state++;
+		state = state < sequenceOfPlay.Length ? state : 0;
+		string stateText = sequenceOfPlay [state];
+		if (sequenceDisplayText != null) {
+			sequenceDisplayText.GetComponent<Text> ().text = stateText;
+			Invoke (stateText, 0f);
+		}
+		Invoke ("NextState", 1f);
+	}
+
+
+	void ClearBattleField() {
+		Debug.Log ("ClearBattleField");
+	}
+
+	void TitleScreen() {
+		Debug.Log ("TitleScreen");
+	}
+
+	void GenerateBattleField() {
+		Debug.Log ("GenerateBattleField");
+	}
+
+	void BattleFieldFlyOver() {
+		Debug.Log ("BattleFieldFlyOver");
+	}
+
+	void Bidding() {
+		Debug.Log ("Bidding");
+	}
+
+	void StartSimulation() {
+		Debug.Log ("StartSimulation");
+	}
+
+	void EndSimulation() {
+		Debug.Log ("EndSimulation");
+	}
+
+	void DistributeWin() {
+		Debug.Log ("DistributeWin");
+	}
+
+
+}
