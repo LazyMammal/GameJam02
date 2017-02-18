ac25e9ba6d6d3f77f4b70f2ecde9153f4287675e
Dennis Carlstrom <dennis.carlstrom@gmail.com>
2017-02-17 22:46:42 -0500

    test world

---
+++ Assets/ProjectileSpawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileSpawn : MonoBehaviour {
+
+	public float speed = 22f;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody>();
+		rb.AddForce( transform.forward * speed, ForceMode.VelocityChange );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
