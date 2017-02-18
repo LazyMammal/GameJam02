402aa8050a71056a5bdbb0f7c7f5e6ddb4e16989
Dennis Carlstrom <dennis.carlstrom@gmail.com>
2017-02-18 00:42:48 -0500

    move scripts into Scripts folder

---
+++ Assets/Scripts/SpawnItems.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class SpawnItems : MonoBehaviour
+{
+	public float radius = 15f;
+	public int count = 1;
+
+	public GameObject prefabs;
+
+    void Start()
+    {
+
+
+    }
+}
