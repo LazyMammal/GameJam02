2b56e2fb7877f28ca3611bf3ee246a7a270a57cf
panchishin <anton@panchishin.com>
2017-02-18 00:50:38 -0500

    Create wall via script

---
+++ Assets/Scripts/PlaceWall.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceWall : MonoBehaviour {
+
+	public GameObject wall_prefab;
+	public int x_size = 1;
+	public int y_size = 3;
+	public int z_size = 10;
+	public float forward_distance = 10f;
+
+	// Use this for initialization
+	void Start () {
+		BuildWall ();		
+	}
+
+	public void PlaceSingleWall(int x, int y, int z) {
+		Vector3 offset = new Vector3 (x, y, z);
+		Instantiate (wall_prefab, 
+			gameObject.transform.position + gameObject.transform.forward * forward_distance + offset, 
+			gameObject.transform.rotation );
+	}
+
+	public void BuildWall() {
+		for (int x = 0; x < x_size; x++) {
+			for (int y = 0; y < y_size; y++) {
+				for (int z = 0; z < z_size; z++) {
+					PlaceSingleWall (x, y, z - z_size/2);
+				}
+			}
+		}
+	}
+
+}
