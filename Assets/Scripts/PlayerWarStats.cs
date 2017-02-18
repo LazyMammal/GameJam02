dd7c7d131a7e325db6bbf91ddfc29ad1a0129c17
panchishin <anton@panchishin.com>
2017-02-18 11:00:54 -0500

    Mock up castle bitting ui

---
+++ Assets/Scripts/PlayerWarStats.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWarStats {
+
+	// what castle do i use?
+	public GameObject castleSpawnPoint;
+
+	// what panel do i update?
+	public GameObject PlayerPanel;
+
+	// how many flags are alive?
+	int flags_remaining = 0;
+
+	// how many flags have i lost?
+	int flags_lost = 0;
+
+	public PlayerWarStats( GameObject panel, GameObject castle, int initial_flags ) {
+		PlayerPanel = panel;
+		castleSpawnPoint = castle;
+		flags_remaining = initial_flags;
+		flags_lost = 0;
+	}
+
+	public void FlagDestroyed( ) {
+		flags_remaining--;
+		flags_lost++;
+	}
+
+	public int FlagRemaining() {
+		return flags_remaining;
+	}
+
+	public int FlagsLost() {
+		return flags_lost;
+	}
+
+}
