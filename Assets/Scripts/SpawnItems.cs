24e4f2125e7a036034db94886a0449ef1e0440b4
Dennis Carlstrom <dennis.carlstrom@gmail.com>
2017-02-18 01:02:54 -0500

    create SpawnItems script for flag spawning

--- Assets/Scripts/SpawnItems.cs
+++ Assets/Scripts/SpawnItems.cs
@@ -2,14 +2,23 @@
 
 public class SpawnItems : MonoBehaviour
 {
-	public float radius = 15f;
-	public int count = 1;
-
-	public GameObject prefabs;
+    public float radius = 15f;
+    public int count = 1;
+    public GameObject[] prefabs;
+    public bool randomizeRotation = false;
 
     void Start()
     {
-
-
+        foreach (var item in prefabs)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Quaternion rot = randomizeRotation ? Quaternion.Euler(0, Random.value * 360, 0) : transform.rotation;
+                Vector2 pos2 = Random.insideUnitCircle;
+                Vector3 pos3 = new Vector3(pos2.x, 0, pos2.y);
+                pos3 *= radius;
+                GameObject go = (GameObject)Instantiate(item, transform.position + pos3, rot);
+            }
+        }
     }
 }
