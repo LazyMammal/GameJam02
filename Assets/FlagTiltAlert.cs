db7617165b3ee8006340bd03aa93882f5cce9aaf
Dennis Carlstrom <dennis.carlstrom@gmail.com>
2017-02-17 23:08:53 -0500

    set prefab parent Y position to zero; placeholder for flag tilt

---
+++ Assets/FlagTiltAlert.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagTiltAlert : MonoBehaviour
+{
+
+    public float tiltAngle = 45;
+
+    // Called for physics updates
+    void FixedUpdate()
+    {
+		// get angle from vertical
+
+		
+
+    }
+}
