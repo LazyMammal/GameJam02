0da938d50a9c449c3fd5c2e72c39bd6a1ac5b05e
panchishin <anton@panchishin.com>
2017-02-19 10:43:00 -0500

    Make Wall Big and Small

--- Assets/Scripts/PlaceWall.cs
+++ Assets/Scripts/PlaceWall.cs
@@ -6,24 +6,26 @@ public class PlaceWall : MonoBehaviour, CommandInterface {
 
 	public GameObject wall_prefab;
 	public int x_size = 1;
-	public int y_size = 3;
-	public int z_size = 10;
-	public float forward_distance = 10f;
+	public int y_size = 10;
+	public int z_size = 50;
+	public int forward_distance = 10;
 
 
-	public void PlaceSingleWall(int x, int y, int z) {
+	public void PlaceSingleWall(int x, int y, int z, int forward) {
 		Vector3 offset = new Vector3 (x, y, z);
 		GameObject wall = (GameObject) Instantiate (wall_prefab, 
-			gameObject.transform.position + gameObject.transform.forward * forward_distance + offset, 
+			gameObject.transform.position + gameObject.transform.forward * forward + offset, 
 			gameObject.transform.rotation );
 		wall.transform.SetParent(transform);
 	}
 
 	public void DoCommand() {
+		int maxY = Random.Range (1, y_size);
+		int maxZ = Random.Range (1, z_size);
 		for (int x = 0; x < x_size; x++) {
-			for (int y = 0; y < y_size; y++) {
-				for (int z = 0; z < z_size; z++) {
-					PlaceSingleWall (x, y, z - z_size/2);
+			for (int y = 0; y < maxY; y++) {
+				for (int z = 0; z < maxZ; z++) {
+					PlaceSingleWall (x, y, z - maxZ/2, forward_distance);
 				}
 			}
 		}
