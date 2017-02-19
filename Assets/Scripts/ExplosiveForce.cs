1fd45dfe62a3b3caab33fbe4d80b9c3d71003c23
Dennis Carlstrom <dennis.carlstrom@gmail.com>
2017-02-19 14:54:26 -0500

    zap ratio in explosion

--- Assets/Scripts/ExplosiveForce.cs
+++ Assets/Scripts/ExplosiveForce.cs
@@ -5,22 +5,29 @@ public class ExplosiveForce : MonoBehaviour
     public float radius = 5.0F;
     public float power = 10.0F;
 
+	public float zapRatio = 0.5f;
+
     void OnCollisionEnter(Collision collision)
     {
         Vector3 explosionPos = transform.position;
         Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
         foreach (Collider hit in colliders)
         {
-            if (hit.tag != "Projectile")
+            if (hit.transform.parent && hit.transform.parent.tag == "Wall")
             {
-                Rigidbody rb = hit.GetComponent<Rigidbody>();
+                Rigidbody rb = hit.transform.parent.GetComponent<Rigidbody>();
 
                 if (rb != null)
-                    rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
+				{
+					if( Random.Range(0f,1f) > zapRatio )
+                    	rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
+					else
+						rb.gameObject.SetActive(false);
+				}
             }
         }
 
-        Object.Destroy(gameObject);
+        //Object.Destroy(gameObject);
     }
 
 }
