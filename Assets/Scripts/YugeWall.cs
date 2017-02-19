f9b9c56dbedfefb3e9ed3ca2ec7c1bc19518c0b6
Dennis Carlstrom <dennis.carlstrom@gmail.com>
2017-02-19 11:37:02 -0500

    YugeWall is stable now

--- Assets/Scripts/YugeWall.cs
+++ Assets/Scripts/YugeWall.cs
@@ -12,11 +12,23 @@ public class YugeWall : MonoBehaviour, CommandInterface
     private GameObject[,] blockRefs;
     private int wallHead = 0, wallTail = 0, wallLength = 0;
     private bool isPaused = true;
+    private Vector3 blockSize = Vector3.one;
+    public Vector3 blockPadding = new Vector3(0.05f, 0.05f, 0.05f);
 
     void Start()
     {
         wallHeight = new int[maxLength];
         blockRefs = new GameObject[maxLength, maxHeight];
+
+        if (block_prefab == null)
+           block_prefab = Instantiate(Resources.Load("YugeWallBlock", typeof(GameObject))) as GameObject;
+
+        BoxCollider bc = block_prefab.GetComponentInChildren<BoxCollider>();
+        if( bc )
+            blockSize = bc.transform.localScale;
+
+        // add some padding
+        blockSize += blockPadding;
     }
 
     public void DoCommand()
@@ -145,8 +157,48 @@ public class YugeWall : MonoBehaviour, CommandInterface
                 EraseColumnAtTail();
             }
         }
-        CreateBlock(wallHead, 0, head.position + transform.right, head.rotation);
+
+        // create base level block
+        CreateBlock(wallHead, 0, head.position + transform.right * blockSize.x, head.rotation);
         wallLength++;
+
+        // create additional blocks in previous columns
+        for (int level = 1; level < maxHeight && level < wallLength; level++)
+        {
+            GenerateBlockLayer((wallHead - level + maxLength) % maxLength);
+        }
+    }
+
+    void GenerateBlockLayer(int x)
+    {
+        if (x >= 0 && x < maxLength)
+        {
+            // check if wall height has already been set
+            int height = wallHeight[x];
+            if (height <= 0)
+            {
+                // get wall height based on math function
+                wallHeight[x] = height = (int)Mathf.Clamp(1f + maxHeight * Mathf.Sin((float)x / maxLength * Mathf.PI * 2f), 1f, maxHeight);
+            }
+
+            // get reference to base block
+            var block = blockRefs[x, 0];
+            if (block && block.activeSelf)
+            {
+                // add at most one block
+                for (int y = 1; y < height && y < maxHeight; y++)
+                {
+                    Vector3 stagger = transform.right * 0.5f * (float)(y%2);
+                    Vector3 elevation = transform.up * blockSize.y * y;
+                    if (!blockRefs[x, y] || !blockRefs[x, y].activeSelf)
+                    {
+                        CreateBlock(x, y, block.transform.position + elevation + stagger, block.transform.rotation);
+                        break;
+                    }
+                }
+            }
+
+        }
     }
 }
 
